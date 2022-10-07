9d25fc8de3dff1bdf1ff64679fc2b32ff65e191d
Jeferson Ferreira <jerfinho.ferreira@gmail.com>
2022-10-06 21:03:31 -0300

    criacao da navigation controller

--- novoExercicioNavegacao/novoExercicioNavegacao/ViewController.swift
+++ novoExercicioNavegacao/novoExercicioNavegacao/ViewController.swift
@@ -8,15 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+  
     @IBAction func tappedButton(_ sender: UIButton) {
         performSegue(withIdentifier: "tela02", sender: nil)
     }
+    
     
 }
 
